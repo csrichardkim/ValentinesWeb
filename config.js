6553743d2b26323a997203f4a239f0e57540c79d
Richard Kim <166664193+csrichardkim@users.noreply.github.com>
2026-02-08 12:41:01 -1000

    Update emojis

--- config.js
+++ config.js
@@ -9,12 +9,12 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Can You Be My Valentine? 💝",
+    pageTitle: "Can You Be My Valentine? 🧸",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
+        hearts: ['❤️', '💖', '💝', '💗', '🐱'],  // Heart emojis
         bears: ['🧸', '🐻']                       // Cute bear emojis
     },
 
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Will you be my Valentine on February 14th, 2025? 🧸", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -42,16 +42,16 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "YAYYAYAYAY 🥰🧸🧸",  // Shows when they go past 5000%
+        high: "OOOOOOOOOOOH! 🚀🧸",              // Shows when they go past 1000%
+        normal: "HEHEHEHEHEH! 🧸"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        title: "Yayyy!!! 🧸💝💖💝🧸",
+        message: "I'll see you on the 14th!! ",
+        emojis: "🧸💖💝💋❤️💕🧸"  // These will bounce around
     },
 
     // Color scheme for the website
