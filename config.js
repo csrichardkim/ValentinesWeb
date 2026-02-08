c60d7ff9d30fec5da5933ebb88ae3e0dc4c4c346
Richard Kim <166664193+csrichardkim@users.noreply.github.com>
2026-02-08 11:47:20 -1000

    Update music URL in config.js

--- config.js
+++ config.js
@@ -62,7 +62,7 @@ animations: {
 music: {
     enabled: true, // Music feature is enabled
     autoplay: true, // Try to autoplay (note: some browsers may block this)
-    musicUrl: "https://open.spotify.com/track/254bXAqt3zP6P50BdQvEsq?si=76d147c825eb4789", // Paste your music URL here
+    musicUrl: "https://res.cloudinary.com/dit5qbmly/video/upload/v1770587130/M%C3%83_neskin_-_IF_NOT_FOR_YOU_Lyrics_vzfmog.mp3", // Paste your music URL here
     startText: "🎵 Play Music", // Button text to start music
     stopText: "🔇 Stop Music", // Button text to stop music
     volume: 0.5 // Volume level (0.0 to 1.0)
